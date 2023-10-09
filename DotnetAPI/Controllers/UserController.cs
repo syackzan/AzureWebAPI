c136d56aab8f09dfb304efab55a880ed5c53e1f5
Scotty Yackzan <syackzan@gmail.com>
2023-10-09 11:42:16 +0200

    Added custom controller for user

---
+++ DotnetAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+
+    public UserController()
+    {
+    
+    }
+
+    //This will set up /User/GetUsers
+    [HttpGet("GetUsers/{testValue}")]
+
+    //public IEnumerable<User> GetUsers()
+    public string[] GetUsers(string testValue)
+    {
+        return new string[] {"user1", "user2", testValue};
+
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+    }
+}
